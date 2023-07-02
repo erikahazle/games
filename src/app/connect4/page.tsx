9bd7aed6eb3c741fb739282bf5976c190d030226
erikajon <hello@jonika.io>
2023-07-02 17:28:12 +0100

    Next app setup + page for connect4

---
+++ src/app/connect4/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Connect4() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>Connect 4 will go here</div>
+    </main>
+  )
+}
