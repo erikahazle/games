ad3b264245c9e7c726b058d3fa9fac061ea42134
erikajon <hello@jonika.io>
2023-07-03 09:28:20 +0100

    Test environment setup

---
+++ src/app/connect4/lib/checkForWinner/index.spec.tsx
@@ -0,0 +1,25 @@
+import { checkForWinner } from '.';
+
+/**
+ * board
+ * [
+ *  [null, null, null, null, null]
+ *  [null, null, null, null, null]
+ *  [null, null,   P1, null, null]
+ *  [null, null, null, null, null]
+ *  [null, null, null, null, null]
+ * ]
+ * 
+ * isVerticalMatch
+ * isHorizontalMatch
+ * isMatchAcrossLeftToRight
+ * isMatchAcrossRightToLeft
+*/
+
+describe('checkForWinner()', () => {
+  describe('when there is a winner match vertically', () => {
+    it('should return true', () => {
+      expect(false).toBe(true);
+    });
+  });
+});
